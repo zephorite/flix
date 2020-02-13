6303fc08f7c94b318379d28e055db4bdc61edcef
Baraa Hegazy <hegazyba@msu.edu>
2020-02-12 22:03:09 -0500

    commit

---
+++ flix/VideoViewController.swift
@@ -0,0 +1,31 @@
+//
+//  VideoViewController.swift
+//  flix
+//
+//  Created by Baraa Hegazy on 2/12/20.
+//  Copyright © 2020 BaraaHegazy. All rights reserved.
+//
+
+import UIKit
+
+class VideoViewController: UIViewController {
+    @IBOutlet weak var webView: WKWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
